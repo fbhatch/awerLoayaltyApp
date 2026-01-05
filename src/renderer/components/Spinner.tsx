4eea7057d9e00a9065157523a526b0f565c33b56
Felipe Baranovsky <fbaranovskyhtch@gmail.com>
2026-01-05 10:56:59 -0300

    Update Spinner.tsx

--- src/renderer/components/Spinner.tsx
+++ src/renderer/components/Spinner.tsx
@@ -5,7 +5,7 @@ interface SpinnerProps {
 }
 
 const Spinner: React.FC<SpinnerProps> = ({ className = '' }) => (
-  <div className={`animate-spin rounded-full h-12 w-12 border-4 border-green-500 border-t-transparent ${className}`}></div>
+  <div className={`block mx-auto animate-spin rounded-full h-12 w-12 border-4 border-green-500 border-t-transparent ${className}`}></div>
 );
 
 export default Spinner;
